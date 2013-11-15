e36e1394a2e6ded8b8087473056cab7c29a342fe
Phil Lee <asmega@ph-lee.com>
2013-11-15 21:04:31 +0000

    implemented fav language

--- lib/marmite/user.rb
+++ lib/marmite/user.rb
@@ -3,7 +3,9 @@ require 'octokit'
 module Marmite
   class User
     def initialize(username)
-      @user = ::Octokit.user username
+      client = Octokit::Client.new :access_token => "token"
+
+      @user = client.user username
     end
 
     def email
@@ -12,6 +14,20 @@ module Marmite
 
     def repos
       @repos ||= user.rels[:repos].get.data
+    end
+
+    def languages
+      hash = {}
+
+      repos.map(&:language).compact.each do |lang|
+        hash[lang] = (hash[lang] || 0) + 1
+      end
+
+      hash
+    end
+
+    def favourite_language
+      languages.max{|a,b| a.last <=> b.last}.first
     end
 
     private
--- spec/lib/user_spec.rb
+++ spec/lib/user_spec.rb
@@ -16,4 +16,20 @@ describe Marmite::User do
       subject.repos.map(&:name).should include("dotfiles")
     end
   end
+
+  describe :languages do
+    it 'returns some languages' do
+      subject.languages.should == {
+        "Erlang" => 1,
+        "JavaScript" => 3,
+        "Ruby" => 22,
+        "VimL" => 1 }
+    end
+  end
+
+  describe :favourite_language do
+    it 'returns Ruby' do
+      subject.favourite_language.should == "Ruby"
+    end
+  end
 end
